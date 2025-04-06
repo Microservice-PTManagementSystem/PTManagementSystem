d2b6ac6e98a102c3921bc474f7bf43c7c30eba1f
DorukYelken <dorukyelken@gmail.com>
2025-04-07 01:48:25 +0300

    datetime_serializer added

--- appointment/app/services/slot_services/slot_reserved_event.py
+++ appointment/app/services/slot_services/slot_reserved_event.py
@@ -1,16 +1,23 @@
 import pika
 import json
- 
+from datetime import datetime
+
+def datetime_serializer(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat()  # Converts datetime to ISO format string
+    raise TypeError("Type not serializable")
+
 def slot_reserved_event(reservation_data):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
     channel = connection.channel()
- 
-    channel.queue_declare(queue='reservationQueue')  
 
-    
-    message = json.loads(reservation_data)
+    channel.queue_declare(queue='reservationQueue')
+
+    # Use custom serializer for datetime objects
+    message = json.dumps(reservation_data, default=datetime_serializer)
 
     channel.basic_publish(exchange='', routing_key='reservationQueue', body=message)
- 
+
     print(" [x] Sent 'reservationMade' event")
     connection.close()
+
