7e92632f08b484252fb680a7bd89448352a10ba7
Sezen Huddamoğlu <sezenhuddam@gmail.com>
2025-04-20 23:25:18 +0300

    event fix

--- payment/paymentDemo/src/main/java/com/example/paymentDemo/config/RabbitMQConfig.java
+++ payment/paymentDemo/src/main/java/com/example/paymentDemo/config/RabbitMQConfig.java
@@ -1,59 +1,3 @@
-/*package com.example.paymentDemo.config;
-
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RabbitMQConfig {
-
-    public static final String PAYMENT_QUEUE = "slot.reserved.queue";
-    public static final String PAYMENT_SUCCESS_QUEUE = "payment.success.queue";
-    public static final String PAYMENT_FAILED_QUEUE = "payment.failed.queue";
-    public static final String EXCHANGE = "pt.topic.exchange";
-
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(EXCHANGE);
-    }
-
-    @Bean
-    public Queue paymentQueue() {
-        return new Queue(PAYMENT_QUEUE);
-    }
-
-    @Bean
-    public Queue paymentSuccessQueue() {
-        return new Queue(PAYMENT_SUCCESS_QUEUE);
-    }
-
-    @Bean
-    public Queue paymentFailedQueue() {
-        return new Queue(PAYMENT_FAILED_QUEUE);
-    }
-
-    @Bean
-    public Binding slotReservedBinding() {
-        return BindingBuilder.bind(paymentQueue()).to(exchange()).with("slot.reserved");
-    }
-
-    @Bean
-    public Binding paymentSuccessBinding() {
-        return BindingBuilder.bind(paymentSuccessQueue()).to(exchange()).with("payment.success");
-    }
-
-    @Bean
-    public Binding paymentFailedBinding() {
-        return BindingBuilder.bind(paymentFailedQueue()).to(exchange()).with("payment.failed");
-    }
-}
-
-
-
-*/
 package com.example.paymentDemo.config;
 
 import org.springframework.amqp.core.Binding;
@@ -72,11 +16,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 @Configuration
 public class RabbitMQConfig {
 
-    //public static final String RESERVATION_QUEUE = "reservationQueue";
-
-    /*public static final String PAYMENT_QUEUE = "slot.reserved.queue";
-    public static final String PAYMENT_SUCCESS_QUEUE = "payment.success.queue";
-    public static final String PAYMENT_FAILED_QUEUE = "payment.failed.queue";*/
+  
     public static final String EXCHANGE = "pt.topic.exchange";
 
 
@@ -101,27 +41,18 @@ public class RabbitMQConfig {
         return new TopicExchange(EXCHANGE);
     }   
 
-    
-   /* @Bean
-    public Queue paymentQueue() {
-        return new Queue("paymentQueue", true); // durable olsun
-    }
-
-    @Bean
-    public Binding paymentQueueBinding(Queue paymentQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(paymentQueue).to(exchange).with("payment.#");
-    } */
-
     @Bean
     public Queue reservationQueue() {
         return new Queue("reservationQueue", false); // durable olsun
     }
-
     @Bean
-    public Binding reservationQueueBinding(Queue reservationQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(reservationQueue).to(exchange).with("reservation.created");
+    public Queue PaymentSucceededQueue() {
+        return new Queue("PaymentSucceededEvent", true);
     }
-    
 
+     @Bean
+     public Binding reservationQueueBinding(Queue reservationQueue, TopicExchange exchange) {
+         return BindingBuilder.bind(reservationQueue).to(exchange).with("reservation.created");
+     }
     
 }
