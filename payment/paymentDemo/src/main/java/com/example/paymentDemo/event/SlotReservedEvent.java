f930e21f6341499a1b060e79945fc1018217672b
aylin <aylingul0107@gmail.com>
2025-04-20 17:31:15 +0300

    feat: changes from main moved to paymentService

--- payment/paymentDemo/src/main/java/com/example/paymentDemo/event/SlotReservedEvent.java
+++ payment/paymentDemo/src/main/java/com/example/paymentDemo/event/SlotReservedEvent.java
@@ -2,60 +2,55 @@ package com.example.paymentDemo.event;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class SlotReservedEvent implements Serializable {
-    private String id;
-    private String appointmentDate;
-    private String appointmentTime;
-    private String createdDate;
-    private String trainerId;
-    private String customerId;
+    @JsonProperty("slot_id")
+    private String slotId;
+
+    @JsonProperty("user_id")
+    private String userId;
+
+    @JsonProperty("timestamp")
+    private String timestamp;
+    //private String appointment_time;
+    //private String appointment_date;
+    //private String created_date;
+    //private String trainer_id;
+    //private String customer_id; bi de id için var eklenmiş
+
+    public SlotReservedEvent() {
+    }   
+
+    public SlotReservedEvent(String slotId,String userId,String timestamp ) {
+        this.userId =userId;
+        this.timestamp = timestamp;
+        this.slotId=slotId;
+
+
+    }
+
+    
 
     // Getters and Setters
-    public String getId() {
-        return id;
+    public String getSlotId() {
+        return slotId;
+    }   
+    public void setSlotId(String slotId) {
+        this.slotId = slotId;
+    }   
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }   
+
+    public String getTimestamp() {
+        return timestamp;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getAppointmentDate() {
-        return appointmentDate;
-    }
-
-    public void setAppointmentDate(String appointmentDate) {
-        this.appointmentDate = appointmentDate;
-    }
-
-    public String getAppointmentTime() {
-        return appointmentTime;
-    }
-
-    public void setAppointmentTime(String appointmentTime) {
-        this.appointmentTime = appointmentTime;
-    }
-
-    public String getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(String createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public String getTrainerId() {
-        return trainerId;
-    }
-
-    public void setTrainerId(String trainerId) {
-        this.trainerId = trainerId;
-    }
-
-    public String getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
     }
 }
--- payment/paymentDemo/src/main/java/com/example/paymentDemo/service/PaymentService.java
+++ payment/paymentDemo/src/main/java/com/example/paymentDemo/service/PaymentService.java
@@ -2,28 +2,11 @@ package com.example.paymentDemo.service;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
-/* @Service
-public class PaymentService {
-
-    private final RabbitTemplate rabbitTemplate;
-
-    public PaymentService(RabbitTemplate rabbitTemplate) {
-        this.rabbitTemplate = rabbitTemplate;
-    }
-
-    public void sendPaymentMessage(String message) {
-        rabbitTemplate.convertAndSend("payment_queue", message);
-        System.out.println("Payment message sent: " + message);
-    }
-}
-*/
 
 import com.example.paymentDemo.config.RabbitMQConfig;
 import com.example.paymentDemo.event.PaymentFailedEvent;
 import com.example.paymentDemo.event.PaymentSucceededEvent;
 import com.example.paymentDemo.event.SlotReservedEvent;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
@@ -39,15 +22,15 @@ public class PaymentService {
 
         if (paymentSuccess) {
             PaymentSucceededEvent successEvent = new PaymentSucceededEvent();
-            successEvent.setReservationId(event.getId());
-            successEvent.setTrainerId(event.getTrainerId());
-            successEvent.setCustomerId(event.getCustomerId());
+            successEvent.setReservationId(event.getSlotId());
+            //successEvent.setTrainerId(event.getTrainerId());
+           // successEvent.setCustomerId(event.getCustomerId());
 
             rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE, "payment.success", successEvent);
         } else {
             PaymentFailedEvent failedEvent = new PaymentFailedEvent();
-            failedEvent.setReservationId(event.getId());
-            failedEvent.setCustomerId(event.getCustomerId());
+            failedEvent.setReservationId(event.getSlotId());
+            //failedEvent.setCustomerId(event.getCustomerId());
 
             rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE, "payment.failed", failedEvent);
         }
