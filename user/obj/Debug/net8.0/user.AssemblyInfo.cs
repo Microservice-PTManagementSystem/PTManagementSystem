1b215ff49ee4b16dab35ae425651e7b865ca3d0e
Sezen Huddamoğlu <sezenhuddam@gmail.com>
2025-04-29 15:39:20 +0300

    fix

--- user/obj/Debug/net8.0/user.AssemblyInfo.cs
+++ user/obj/Debug/net8.0/user.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("user")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35786ebea0c2ce77d5964f0b86a488b2a1674a7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb701c64a484de9bfd5cef9a6af0dd5236386aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("user")]
 [assembly: System.Reflection.AssemblyTitleAttribute("user")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
