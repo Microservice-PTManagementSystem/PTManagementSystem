f7b5bd28da4578aaa7d24b943a23c0bdbb08fdd5
Aylin Gül <aylingul0107@gmail.com>
2025-05-04 17:52:21 +0300

    unit tests added on payment

--- payment/paymentDemo/src/main/java/com/example/paymentDemo/controller/PaymentController.java
+++ payment/paymentDemo/src/main/java/com/example/paymentDemo/controller/PaymentController.java
@@ -1,14 +1,62 @@
 package com.example.paymentDemo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.paymentDemo.dto.PaymentRequest;
+import com.example.paymentDemo.dto.PaymentResult;
+import com.example.paymentDemo.model.Payment;
+import com.example.paymentDemo.model.PaymentStatus;
+import com.example.paymentDemo.service.PaymentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
+@RequestMapping("/payment")
 public class PaymentController {
+
+    private final PaymentService paymentService;
+    public PaymentController(PaymentService paymentService) { 
+        this.paymentService = paymentService; }
+
 
     @GetMapping("/payment/health")
     public String checkHealth() {
         return "Payment Service is running!";
     }
+
+    @PostMapping("/initiate")
+    public ResponseEntity<Payment> initiatePayment(@RequestBody PaymentRequest request) {
+        Payment payment = paymentService.initiatePayment(request);
+        return ResponseEntity.ok(payment);
+    }
+
+    @PostMapping("/confirm")
+    public ResponseEntity<Payment> confirmPayment(@RequestBody PaymentResult result) {
+        Payment payment = paymentService.confirmPayment(result);
+        return ResponseEntity.ok(payment);
+    }
+
+    @PostMapping("/retry/{paymentId}")
+    public ResponseEntity<Payment> retryPayment(@PathVariable Long paymentId) {
+        Payment payment = paymentService.retryPayment(paymentId);
+        return ResponseEntity.ok(payment);
+    }
+
+    @PostMapping("/refund/{paymentId}")
+    public ResponseEntity<Payment> issueRefund(@PathVariable Long paymentId) {
+        Payment payment = paymentService.issueRefund(paymentId);
+        return ResponseEntity.ok(payment);
+    }
+    @GetMapping("/status/{id}")
+public ResponseEntity<PaymentStatus> getStatus(@PathVariable Long id) {
+    try {
+        PaymentStatus status = paymentService.getStatus(id);
+        return ResponseEntity.ok(status);
+    } catch (IllegalArgumentException e) {
+        return ResponseEntity.notFound().build();
+    }
+}
+
+    
 }
 
---
+++ payment/paymentDemo/src/main/java/com/example/paymentDemo/model/PaymentStatus.java
@@ -0,0 +1,8 @@
+package com.example.paymentDemo.model;
+
+public enum PaymentStatus {
+    PENDING,
+    COMPLETED,
+    FAILED,
+    REFUNDED
+}
