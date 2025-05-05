1ac6a11525fa1ebf7ad230bbcbf25ca83e2052f1
Aylin Gül <103492448+AylinGul01@users.noreply.github.com>
2025-05-05 09:48:50 +0300

    Revert "Payment test"

--- payment/paymentDemo/src/main/java/com/example/paymentDemo/event/PaymentSucceededEvent.java
+++ payment/paymentDemo/src/main/java/com/example/paymentDemo/event/PaymentSucceededEvent.java
@@ -1,25 +1,11 @@
 package com.example.paymentDemo.event;
 
-import java.time.LocalDateTime;
+import java.io.Serializable;
 
-import com.example.paymentDemo.model.Payment;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
 public class PaymentSucceededEvent {
     private String slotId;
     private String trainerId;
     private String customerId;
-
-    private Long paymentId;
-    private Double amount;
-    private String method;
-    private LocalDateTime timestamp;
 
     public PaymentSucceededEvent(String slotId, String trainerId, String customerId) {
         this.slotId = slotId;
@@ -27,10 +13,27 @@ public class PaymentSucceededEvent {
         this.customerId = customerId;
     }
 
-    public PaymentSucceededEvent(Payment payment) {
-        this.paymentId = payment.getId();
-        this.amount = payment.getAmount();
-        this.method = payment.getMethod();
-        this.timestamp = LocalDateTime.now();
+    public String getslotId() {
+        return slotId;
+    }
+
+    public void setslotId(String slotId) {
+        this.slotId = slotId;
+    }
+
+    public String getTrainerId() {
+        return trainerId;
+    }
+
+    public void setTrainerId(String trainerId) {
+        this.trainerId = trainerId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
     }
 }
