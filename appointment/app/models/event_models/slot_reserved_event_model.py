61c240e5e329619a1024ff6d099316afa5a95a3d
DorukYelken <dorukyelken@gmail.com>
2025-05-11 18:09:35 +0300

    slot_reserved_event_model updated

--- appointment/app/models/event_models/slot_reserved_event_model.py
+++ appointment/app/models/event_models/slot_reserved_event_model.py
@@ -3,6 +3,16 @@ from pydantic import BaseModel
 
 
 class slot_reserved_event_model(BaseModel):
+    cardNumber : str
+    message: str
+    cardNumber: str
+    cardHolder: str
+    expiryMonth: str
+    expiryYear: str
+    cvc: str
+    saveCard : bool
+    totalAmount : str
+    paymentMethod : str
     slot_id: str
     user_id: str
     timestamp: datetime = datetime.utcnow()
