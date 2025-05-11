75ffb9135d5e5f3c138c3df723dd5b426bcfdee4
Sezen Huddamoğlu <sezenhuddam@gmail.com>
2025-05-11 13:06:51 +0300

    register endpoint updated

--- user/Application/UseCases/Commands/RegisterUser.cs
+++ user/Application/UseCases/Commands/RegisterUser.cs
@@ -28,76 +28,80 @@ namespace PTManagementSystem.Application.UseCases.Commands
         }
 
         public async Task RegisterAsync(KeycloakRegisterDto dto)
+
+        
         {
+            Console.WriteLine("GELEN REGISTER TOKEN: " + dto.Token);
+
             //Console.WriteLine("GELEN REGISTER KEYCLOAK EVENT: " + dto.KeycloakUserId);
 
-            // Validation
-            //await _validationService.ValidateUserDoesNotExistAsync(dto.KeycloakUserId);
+            // // Validation
+            // //await _validationService.ValidateUserDoesNotExistAsync(dto.KeycloakUserId);
 
-            // Keycloak user info
-            var keycloakUser = await _keycloakService.GetUserInfoAsync(dto.KeycloakUserId);
-            //Console.WriteLine("GELEN REGISTER KEYCLOAK USER BİLGİLERİ: " + keycloakUser);
-            if (keycloakUser == null)
-            {
-                throw new Exception("Keycloak user not found.");
-            }
+            // // Keycloak user info
+            // var keycloakUser = await _keycloakService.GetUserInfoAsync(dto.KeycloakUserId);
+            // //Console.WriteLine("GELEN REGISTER KEYCLOAK USER BİLGİLERİ: " + keycloakUser);
+            // if (keycloakUser == null)
+            // {
+            //     throw new Exception("Keycloak user not found.");
+            // }
 
-            //Keycloak role
-            var roles = await _keycloakService.GetUserRolesIncludingGroupsAsync(dto.KeycloakUserId);
-            /*Console.WriteLine("GELEN REGISTER KEYCLOAK ROLELERI:");
-            foreach (var role in roles)
-            {
-                Console.WriteLine("- " + role);
-            }*/
-
-
-            if (roles == null || !roles.Any())
-            {
-                throw new Exception("No roles found for this user.");
-            }
+            // //Keycloak role
+            // var roles = await _keycloakService.GetUserRolesIncludingGroupsAsync(dto.KeycloakUserId);
+            // /*Console.WriteLine("GELEN REGISTER KEYCLOAK ROLELERI:");
+            // foreach (var role in roles)
+            // {
+            //     Console.WriteLine("- " + role);
+            // }*/
 
 
-            UserType userType;
-            if (roles.Contains("Trainer"))
-                userType = UserType.TRAINER;
-            else if (roles.Contains("Client"))
-                userType = UserType.CLIENT;
-            else
-                throw new Exception("Invalid user role.");
+            // if (roles == null || !roles.Any())
+            // {
+            //     throw new Exception("No roles found for this user.");
+            // }
 
 
-            var registerDto = new RegisterUserDto
-            {
-                KeycloakUserId = dto.KeycloakUserId,
-                Email = keycloakUser.Email,
-                IsEmailConfirmed = keycloakUser.EmailVerified,
-                UserType = userType
-            };
+            // UserType userType;
+            // if (roles.Contains("Trainer"))
+            //     userType = UserType.TRAINER;
+            // else if (roles.Contains("Client"))
+            //     userType = UserType.CLIENT;
+            // else
+            //     throw new Exception("Invalid user role.");
 
-            var user = new User
-            {
-                Id = Guid.NewGuid(),
-                KeycloakId = registerDto.KeycloakUserId,
-                Email = registerDto.Email,
-                IsEmailConfirmed = registerDto.IsEmailConfirmed,
-                UserType = registerDto.UserType,
-                UserProfile = new UserProfile(),
-                PaymentInfo = new PaymentInfo(),
-                TrainerProfile = new TrainerProfile()
-            };
 
-            var validationResult = await _validationService.ValidateUserExistsAsync(user.KeycloakId);
-            if (!validationResult.IsValid)
-            {
-                var errorMessages = string.Join(", ", validationResult.Errors.Select(e => e.ErrorMessage));
-                throw new ArgumentException($"Invalid user data: {errorMessages}");
-            }
+            // var registerDto = new RegisterUserDto
+            // {
+            //     KeycloakUserId = dto.KeycloakUserId,
+            //     Email = keycloakUser.Email,
+            //     IsEmailConfirmed = keycloakUser.EmailVerified,
+            //     UserType = userType
+            // };
 
-            var result = await _userRepository.AddUserAsync(registerDto);
-            if (!result)
-            {
-                throw new Exception("User could not be registered.");
-            }
+            // var user = new User
+            // {
+            //     Id = Guid.NewGuid(),
+            //     KeycloakId = registerDto.KeycloakUserId,
+            //     Email = registerDto.Email,
+            //     IsEmailConfirmed = registerDto.IsEmailConfirmed,
+            //     UserType = registerDto.UserType,
+            //     UserProfile = new UserProfile(),
+            //     PaymentInfo = new PaymentInfo(),
+            //     TrainerProfile = new TrainerProfile()
+            // };
+
+            // var validationResult = await _validationService.ValidateUserExistsAsync(user.KeycloakId);
+            // if (!validationResult.IsValid)
+            // {
+            //     var errorMessages = string.Join(", ", validationResult.Errors.Select(e => e.ErrorMessage));
+            //     throw new ArgumentException($"Invalid user data: {errorMessages}");
+            // }
+
+            // var result = await _userRepository.AddUserAsync(registerDto);
+            // if (!result)
+            // {
+            //     throw new Exception("User could not be registered.");
+            // }
         }
 
     }
