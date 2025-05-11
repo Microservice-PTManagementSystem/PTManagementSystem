6150fc92b412d0f9199f40e9f4cfefbbc588c408
Sezen Huddamoğlu <sezenhuddam@gmail.com>
2025-05-11 13:09:28 +0300

    register endpoint updated

--- user/Presentation/DTOs/KeycloakRegisterDto.cs
+++ user/Presentation/DTOs/KeycloakRegisterDto.cs
@@ -3,6 +3,6 @@
     public class KeycloakRegisterDto
     {
        // public string KeycloakUserId { get; set; } = string.Empty;
-       public string Token { get; set; };
+       public string Token { get; set; }
     }
 }
